0fa528fbb92550fe7520d2935e1d9dd92f6afd1d
hao_zhang <hao_zhang@Ctrip.com>
2017-03-01 18:52:01 +0800

    saferHTML

---
+++ SaferHTML.js
@@ -0,0 +1,18 @@
+var message =
+    SaferHTML `<p>${sender} has sent you a message.</p>`;
+
+function SaferHTML(templateData) {
+    var s = templateData[0];
+    for (var i = 1; i < arguments.length; i++) {
+        var arg = String(arguments[i]);
+
+        // Escape special characters in the substitution.
+        s += arg.replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;");
+
+        // Don't escape special characters in the template.
+        s += templateData[i];
+    }
+    return s;
+}
