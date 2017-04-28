4f6b76b44467d17fd0556448181cb8ad833ba79d
hao_zhang <hao_zhang@Ctrip.com>
2017-04-28 18:16:50 +0800

    2017/4/28 18:16 公司

--- learn/Meteor/myApp/client/templates/application/layout.js
+++ learn/Meteor/myApp/client/templates/application/layout.js
@@ -1,3 +1,19 @@
 Template.layout.helpers({
-  pageTitle: function() { return Session.get('pageTitle'); }
+    pageTitle: function() {
+        return Session.get('pageTitle'); }
+});
+Template.layout.onRendered(function() {
+    this.find('#main')._uihooks = {
+        insertElement: function(node, next) {
+            $(node)
+                .hide()
+                .insertBefore(next)
+                .fadeIn();
+        },
+        removeElement: function(node) {
+            $(node).fadeOut(function() {
+                $(this).remove();
+            });
+        }
+    }
 });
