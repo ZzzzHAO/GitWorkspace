3bf90a53d4c9e74f3e94f62f7c91643504bc7ee2
hao_zhang <hao_zhang@Ctrip.com>
2017-05-09 19:18:53 +0800

    2017/5/9 19:18 公司

--- learn/aurelia/src/app.ts
+++ learn/aurelia/src/app.ts
@@ -1,12 +1,75 @@
 import {Todo} from './todo';
-
+import Swiper from '../scripts/swiper.min.js'
+    // var appendNumber = 4;
+    // var prependNumber = 1;
+    // var swiper = new Swiper('.swiper-container', {
+    //     pagination: '.swiper-pagination',
+    //     nextButton: '.swiper-button-next',
+    //     prevButton: '.swiper-button-prev',
+    //     slidesPerView: 3,
+    //     centeredSlides: true,
+    //     paginationClickable: true,
+    //     spaceBetween: 30,
+    // });
+    // document.querySelector('.prepend-2-slides').addEventListener('click', function (e) {
+    //     e.preventDefault();
+    //     swiper.prependSlide([
+    //         '<div class="swiper-slide">Slide ' + (--prependNumber) + '</div>',
+    //         '<div class="swiper-slide">Slide ' + (--prependNumber) + '</div>'
+    //     ]);
+    // });
+    // document.querySelector('.prepend-slide').addEventListener('click', function (e) {
+    //     e.preventDefault();
+    //     swiper.prependSlide('<div class="swiper-slide">Slide ' + (--prependNumber) + '</div>');
+    // });
+    // document.querySelector('.append-slide').addEventListener('click', function (e) {
+    //     e.preventDefault();
+    //     swiper.appendSlide('<div class="swiper-slide">Slide ' + (++appendNumber) + '</div>');
+    // });
+    // document.querySelector('.append-2-slides').addEventListener('click', function (e) {
+    //     e.preventDefault();
+    //     swiper.appendSlide([
+    //         '<div class="swiper-slide">Slide ' + (++appendNumber) + '</div>',
+    //         '<div class="swiper-slide">Slide ' + (++appendNumber) + '</div>'
+    //     ]);
+    // });
 export class App {
+  // var appendNumber = 4;
+  //   var prependNumber = 1;
+  //   var swiper = new Swiper('.swiper-container', {
+  //       pagination: '.swiper-pagination',
+  //       nextButton: '.swiper-button-next',
+  //       prevButton: '.swiper-button-prev',
+  //       slidesPerView: 3,
+  //       centeredSlides: true,
+  //       paginationClickable: true,
+  //       spaceBetween: 30,
+  //   });
   constructor() {
     this.heading = "Todos";
     this.todos = [];
     this.todoDescription = '';
   }
 
+   created(owningView, myView) {
+      // Invoked once the component is created...
+   }
+
+   bind(bindingContext, overrideContext) {
+      // Invoked once the databinding is activated...
+   }
+
+   attached(argument) {
+      // Invoked once the component is attached to the DOM...
+   }
+
+   detached(argument) {
+      // Invoked when component is detached from the dom
+   }
+
+   unbind(argument) {
+      // Invoked when component is unbound...
+   }
   addTodo() {
     if (this.todoDescription) {
       this.todos.push(new Todo(this.todoDescription));
