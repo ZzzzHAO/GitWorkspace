171d5d18d80bd74676fcf584e3dcc6d402da6968
Zzzz <Zzzz>
2017-05-10 23:14:29 +0800

    2017/5/10 23:14 宿舍

--- learn/aurelia/src/app.ts
+++ learn/aurelia/src/app.ts
@@ -1,34 +1,34 @@
 import Swiper from '../scripts/swiper.min.js'
 export class App {
-  constructor() {
+  constructor() {}
+
+  created(owningView, myView) {
+    // Invoked once the component is created...
   }
 
-   created(owningView, myView) {
-      // Invoked once the component is created...
-   }
+  bind(bindingContext, overrideContext) {
+    // Invoked once the databinding is activated...
+  }
 
-   bind(bindingContext, overrideContext) {
-      // Invoked once the databinding is activated...
-   }
-
-   attached(argument) {
-      // Invoked once the component is attached to the DOM...
+  attached(argument) {
+    // Invoked once the component is attached to the DOM...
     var swiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-        slidesPerView: 3,
-        centeredSlides: true,
-        paginationClickable: true,
-        spaceBetween: 30,
+      pagination: '.swiper-pagination',
+      nextButton: '.swiper-button-next',
+      prevButton: '.swiper-button-prev',
+      slidesPerView: 2,
+      centeredSlides: true,
+      paginationClickable: true,
+      spaceBetween: 250,
+      effect: 'coverflow',
     });
-   }
+  }
 
-   detached(argument) {
-      // Invoked when component is detached from the dom
-   }
+  detached(argument) {
+    // Invoked when component is detached from the dom
+  }
 
-   unbind(argument) {
-      // Invoked when component is unbound...
-   }
+  unbind(argument) {
+    // Invoked when component is unbound...
+  }
 }
