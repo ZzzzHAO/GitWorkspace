c230e463c898a1ac9134a9b98f1bd7a462c193a0
Zzzz <Zzzz>
2017-05-10 21:25:11 +0800

    2017/5/10 21:25 宿舍

--- learn/aurelia/src/app.ts
+++ learn/aurelia/src/app.ts
@@ -1,54 +1,6 @@
-import {Todo} from './todo';
 import Swiper from '../scripts/swiper.min.js'
-    // var appendNumber = 4;
-    // var prependNumber = 1;
-    // var swiper = new Swiper('.swiper-container', {
-    //     pagination: '.swiper-pagination',
-    //     nextButton: '.swiper-button-next',
-    //     prevButton: '.swiper-button-prev',
-    //     slidesPerView: 3,
-    //     centeredSlides: true,
-    //     paginationClickable: true,
-    //     spaceBetween: 30,
-    // });
-    // document.querySelector('.prepend-2-slides').addEventListener('click', function (e) {
-    //     e.preventDefault();
-    //     swiper.prependSlide([
-    //         '<div class="swiper-slide">Slide ' + (--prependNumber) + '</div>',
-    //         '<div class="swiper-slide">Slide ' + (--prependNumber) + '</div>'
-    //     ]);
-    // });
-    // document.querySelector('.prepend-slide').addEventListener('click', function (e) {
-    //     e.preventDefault();
-    //     swiper.prependSlide('<div class="swiper-slide">Slide ' + (--prependNumber) + '</div>');
-    // });
-    // document.querySelector('.append-slide').addEventListener('click', function (e) {
-    //     e.preventDefault();
-    //     swiper.appendSlide('<div class="swiper-slide">Slide ' + (++appendNumber) + '</div>');
-    // });
-    // document.querySelector('.append-2-slides').addEventListener('click', function (e) {
-    //     e.preventDefault();
-    //     swiper.appendSlide([
-    //         '<div class="swiper-slide">Slide ' + (++appendNumber) + '</div>',
-    //         '<div class="swiper-slide">Slide ' + (++appendNumber) + '</div>'
-    //     ]);
-    // });
 export class App {
-  // var appendNumber = 4;
-  //   var prependNumber = 1;
-  //   var swiper = new Swiper('.swiper-container', {
-  //       pagination: '.swiper-pagination',
-  //       nextButton: '.swiper-button-next',
-  //       prevButton: '.swiper-button-prev',
-  //       slidesPerView: 3,
-  //       centeredSlides: true,
-  //       paginationClickable: true,
-  //       spaceBetween: 30,
-  //   });
   constructor() {
-    this.heading = "Todos";
-    this.todos = [];
-    this.todoDescription = '';
   }
 
    created(owningView, myView) {
@@ -61,6 +13,15 @@ export class App {
 
    attached(argument) {
       // Invoked once the component is attached to the DOM...
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        slidesPerView: 3,
+        centeredSlides: true,
+        paginationClickable: true,
+        spaceBetween: 30,
+    });
    }
 
    detached(argument) {
@@ -70,17 +31,4 @@ export class App {
    unbind(argument) {
       // Invoked when component is unbound...
    }
-  addTodo() {
-    if (this.todoDescription) {
-      this.todos.push(new Todo(this.todoDescription));
-      this.todoDescription = '';
-    }
-  }
-
-  removeTodo(todo) {
-    let index = this.todos.indexOf(todo);
-    if (index !== -1) {
-      this.todos.splice(index, 1);
-    }
-  }
 }
